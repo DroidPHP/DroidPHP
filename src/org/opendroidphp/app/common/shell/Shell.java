615dffcc8d7a86248b845a7bb10eee6a91aecaee
Shushant <phppunk@gmail.com>
2014-05-20 00:58:28 +0530

    Updated copyright

--- src/org/opendroidphp/app/common/shell/Shell.java
+++ src/org/opendroidphp/app/common/shell/Shell.java
@@ -1,12 +1,17 @@
 /*
- * *
- *  * This file is part of DroidPHP
- *  *
- *  * (c) 2014 Shushant Kumar
- *  *
- *  * For the full copyright and license information, please view the LICENSE
- *  * file that was distributed with this source code.
+ * Copyright (C) 2012-2013 Jorrit "Chainfire" Jongma
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.opendroidphp.app.common.shell;
