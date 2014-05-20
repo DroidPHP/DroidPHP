fd7f31a4269a9fcb6228c6a3055336ebd8152c2b
Shushant <phppunk@gmail.com>
2014-05-20 18:10:05 +0530

    revert to simpler implementation

--- src/org/opendroidphp/app/common/utils/ShellOutputFormatter.java
+++ src/org/opendroidphp/app/common/utils/ShellOutputFormatter.java
@@ -1,26 +1,17 @@
 package org.opendroidphp.app.common.utils;
 
+import java.util.Locale;
+
 public class ShellOutputFormatter {
 
-    public static String toHTML(String mString) {
+    public static String toHTML(String fromOutput) {
 
-        String templateView = "</style>.prettyprint {\n" +
-                "        padding: 8px;\n" +
-                "        background-color: #f7f7f9;\n" +
-                "        border: 1px solid #e1e1e8;\n" +
-                "        }</style>" + "<pre class=\"prettyprint\">Hell</pre>";
         /**
          * Replace all repeating whitespace with one single whitespace
          */
 
-        mString = mString.replaceAll("\\s+", " ");
-        // String[] mArrStr = mString.split("\n");
-        // String s = "";
-        // for (int i = 0; i < mArrStr.length; i++) {
-        mString = "<strong>" + mString.trim() + "</strong><br/>";
-
-        // }
-        return mString.trim();
+        fromOutput = fromOutput.replaceAll("\\s+", " ");
+        return String.format(Locale.ENGLISH, fromOutput);
 
     }
 
