5ca7b593b3fc8fb1b0461ae57f3d14abd17c5371
Shushant <phppunk@gmail.com>
2014-07-03 01:22:27 +0530

    moved all command into "server-sh.sh" script

--- src/org/opendroidphp/app/common/tasks/ConnectServer.java
+++ src/org/opendroidphp/app/common/tasks/ConnectServer.java
@@ -15,10 +15,15 @@ import eu.chainfire.libsuperuser.Shell;
 public class ConnectServer implements Runnable {
 
     protected final static String CHANGE_PERMISSION = "/system/bin/chmod 755 ";
+
+    protected final static String SERVER_DAEMON_LIGHTTPD = "lighttpd";
+    protected final static String SERVER_DAEMON_NGINX = "nginx";
+
     protected static String EXTERNAL_DIRECTORY = Environment.getExternalStorageDirectory().getPath() + "/droidphp/";
     protected String SERV_PORT_REGEX = "server.port.*";
     protected String baseShell;
     protected String basePort;
+    protected String serverDaemon;
 
     /**
      * Set shell binary to use. Usually "sh" or "su"
@@ -42,6 +47,22 @@ public class ConnectServer implements Runnable {
         return this;
     }
 
+    /**
+     * Set Server Daemon to use
+     *
+     * @param server server daemon to use
+     */
+
+    public ConnectServer setServer(String server) {
+
+        if (server == null || server.equals("")) {
+            server = SERVER_DAEMON_LIGHTTPD;
+        }
+        serverDaemon = server;
+        return this;
+    }
+
+
     @Override
     public void run() {
         initialize();
@@ -51,36 +72,11 @@ public class ConnectServer implements Runnable {
     protected void initialize() {
         List<String> command = new ArrayList<String>();
 
-
-        command.add(CHANGE_PERMISSION
-                .concat(Constants.LIGHTTPD_SBIN_LOCATION));
-
-        command.add(CHANGE_PERMISSION
-                .concat(Constants.PHP_SBIN_LOCATION));
-
-        command.add(CHANGE_PERMISSION
-                .concat(Constants.MYSQL_DAEMON_SBIN_LOCATION));
-
-        command.add(CHANGE_PERMISSION
-                .concat(Constants.MYSQL_MONITOR_SBIN_LOCATION));
-
-        command.add(CHANGE_PERMISSION
-                .concat(Constants.BUSYBOX_SBIN_LOCATION));
-
-        command.add(CHANGE_PERMISSION
-                .concat(Constants.INTERNAL_LOCATION + "/tmp"));
-
+        command.add(CHANGE_PERMISSION.concat(Constants.INTERNAL_LOCATION + "/scripts/server-sh.sh"));
 
         try {
 
             checkFilesystem();
-
-            createOrRestoreConfiguration(
-                    Constants.LIGTTTPD_CONF_LOCATION, "lighttpd.conf");
-            createOrRestoreConfiguration(
-                    Constants.PHP_INI_LOCATION, "php.ini");
-            createOrRestoreConfiguration(
-                    Constants.MYSQL_INI_LOCATION, "mysql.ini");
 
             if (basePort != null) {
                 changeServerData(basePort);
@@ -91,40 +87,12 @@ public class ConnectServer implements Runnable {
             e.printStackTrace();
         }
 
-        // hack for running process in parallel
-        command.add(String.format(
-                Locale.ENGLISH,
-                "%s -f %s -D 1>/dev/null 1>/dev/null 2>&1 & pid_lighttpd=$!",
-                Constants.LIGHTTPD_SBIN_LOCATION,
-                Constants.PROJECT_LOCATION + "/conf/lighttpd.conf"
-        ));
-
-        command.add(String.format(
-                Locale.ENGLISH,
-                "%s --defaults-file=%s --user=root --language=%s 1>/dev/null 2>&1 & pid_mysql=$!",
-                Constants.MYSQL_DAEMON_SBIN_LOCATION,
-                Constants.PROJECT_LOCATION + "/conf/mysql.ini",
-                Constants.MYSQL_SHARE_DATA_LOCATION + "/mysql/english"
-        ));
-
-        command.add(String.format(
-                Locale.ENGLISH,
-                "%s -b 127.0.0.1:9786 -c %s 1>/dev/null 2>&1 & pid_php=$!",
-                Constants.PHP_SBIN_LOCATION,
-                Constants.PHP_INI_LOCATION
-        ));
-
-//        command.add("php=$pid_php, lighttpd=$pid_lighttpd, mysql=$pid_mysql");
-
-        //PHP Environment Variable
-        String[] envs = new String[]{
-                "PHP_FCGI_CHILDERN=3",
-                "PHP_FCGI_MAX_REQUEST=1000"
-        };
+        String daemon = serverDaemon.equals(SERVER_DAEMON_NGINX) ? "nginx" : "lighttpd";
+        command.add(Constants.INTERNAL_LOCATION + "/scripts/server-sh.sh ".concat(daemon));
 
         String commands[] = new String[command.size()];
         int i = 0;
-        //@TODO: simpler way to convert List to String[]
+        //@TODO: simple way to convert List to String[]
         for (String eachCommand : command) {
             commands[i] = eachCommand;
             i++;
@@ -133,7 +101,7 @@ public class ConnectServer implements Runnable {
 
         if (baseShell == null) baseShell = "sh";
 
-        Shell.run(baseShell, commands, envs, false);
+        Shell.run(baseShell, commands, null, false);
     }
 
     protected void checkFilesystem() {
@@ -142,7 +110,11 @@ public class ConnectServer implements Runnable {
                 Constants.PROJECT_LOCATION + "/logs/lighttpd",
                 Constants.PROJECT_LOCATION + "/logs/mysql",
                 Constants.PROJECT_LOCATION + "/logs/php",
+                Constants.PROJECT_LOCATION + "/logs/nginx",
                 Constants.PROJECT_LOCATION + "/conf",
+                Constants.PROJECT_LOCATION + "/conf/nginx/logs",
+                Constants.PROJECT_LOCATION + "/hosts/nginx",
+                Constants.PROJECT_LOCATION + "/hosts/lighttpd",
                 Constants.PROJECT_LOCATION + "/sessions",
                 Constants.INTERNAL_LOCATION + "/tmp",
                 Constants.EXTERNAL_STORAGE + "/htdocs"
@@ -168,18 +140,5 @@ public class ConnectServer implements Runnable {
         } catch (Exception e) {
 
         }
-    }
-
-    protected void createOrRestoreConfiguration(String confFilename, String fileName) throws Exception {
-
-        File f = new File(EXTERNAL_DIRECTORY + "/conf/" + fileName);
-
-        if (f.exists()) return;
-
-        String confValue = FileUtils.readFileToString(
-                new File(confFilename), "UTF-8");
-
-        FileUtils.writeStringToFile(f, confValue, "UTF-8");
-
     }
 }
