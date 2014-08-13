702689aa6ffbbc8982cb4fcd31025f10206f1abf
Shushant <phppunk@gmail.com>
2014-08-14 01:53:12 +0530

    Adapter for Drawer and Extension

---
+++ src/org/opendroidphp/app/model/NavDrawerItem.java
@@ -0,0 +1,58 @@
+package org.opendroidphp.app.model;
+
+public class NavDrawerItem {
+
+    private String title;
+    private int icon = 0;
+    private String count = "0";
+    // boolean to set visiblity of the counter
+    private boolean isCounterVisible = false;
+
+    public NavDrawerItem(String title) {
+        this.title = title;
+    }
+
+    public NavDrawerItem(String title, int icon) {
+        this.title = title;
+        this.icon = icon;
+    }
+
+    public NavDrawerItem(String title, int icon, boolean isCounterVisible, String count) {
+        this.title = title;
+        this.icon = icon;
+        this.isCounterVisible = isCounterVisible;
+        this.count = count;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIcon() {
+        return this.icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+    public String getCount() {
+        return this.count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public boolean getCounterVisibility() {
+        return this.isCounterVisible;
+    }
+
+    public void setCounterVisibility(boolean isCounterVisible) {
+        this.isCounterVisible = isCounterVisible;
+    }
+}
