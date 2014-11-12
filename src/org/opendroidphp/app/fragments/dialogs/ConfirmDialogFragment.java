a052837e389e000cd4e0b4ca56d45db299cfcd48
Shushant <phppunk@gmail.com>
2014-11-13 00:33:16 +0530

    Improved ConfirmDialog to support factory

--- src/org/opendroidphp/app/fragments/dialogs/ConfirmDialogFragment.java
+++ src/org/opendroidphp/app/fragments/dialogs/ConfirmDialogFragment.java
@@ -1,6 +1,7 @@
 package org.opendroidphp.app.fragments.dialogs;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,25 +15,38 @@ import org.opendroidphp.R;
 
 public class ConfirmDialogFragment extends SherlockDialogFragment {
 
+    private static Context sContext;
     private Dialog dialog;
     private DialogClickEvent event;
-
     private TextView tv_title;
     private TextView tv_message;
-
     private String title;
     private String message;
 
-    public ConfirmDialogFragment create(String title, String message) {
-        this.title = title;
-        this.message = message;
-        return this;
+    public static ConfirmDialogFragment create(Context c, final String title, final String message) {
+        sContext = c;
+        ConfirmDialogFragment fragment = new ConfirmDialogFragment();
+        fragment.setTitle(title);
+        fragment.setMessage(message);
+        return fragment;
     }
 
-    public ConfirmDialogFragment create(String title, String message, DialogClickEvent event) {
-        create(title, message);
-        addClickEventListener(event);
-        return this;
+    public static ConfirmDialogFragment create(Context c, final String title, final String message, final DialogClickEvent event) {
+        sContext = c;
+        ConfirmDialogFragment fragment = new ConfirmDialogFragment();
+        fragment.setTitle(title);
+        fragment.setMessage(message);
+        fragment.addClickEventListener(event);
+        return fragment;
+    }
+
+    public static ConfirmDialogFragment create(Context c, final int titleResId, final int messageResId) {
+        sContext = c;
+        ConfirmDialogFragment fragment = new ConfirmDialogFragment();
+        fragment.setTitle(titleResId);
+        fragment.setMessage(messageResId);
+        //fragment.addClickEventListener(event);
+        return fragment;
     }
 
     @Override
@@ -56,27 +70,46 @@ public class ConfirmDialogFragment extends SherlockDialogFragment {
         Button btnConfirm = (Button) dialog.findViewById(R.id.positive);
         btnConfirm.setText(R.string.confirm);
 
-        if (event != null) {
-            btnNotConfirm.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    dismiss();
-                    event.onConfirmExit();
-                }
-            });
-            btnConfirm.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    dismiss();
-                    event.onConfirm();
-                }
-            });
-        }
+        btnNotConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+                if (event != null) event.onConfirmExit();
+            }
+        });
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+                if (event != null) event.onConfirm();
+            }
+        });
+
         return dialog;
     }
 
     public ConfirmDialogFragment addClickEventListener(DialogClickEvent event) {
         this.event = event;
+        return this;
+    }
+
+    public ConfirmDialogFragment setTitle(final String title) {
+        this.title = title;
+        return this;
+    }
+
+    public ConfirmDialogFragment setMessage(final String message) {
+        this.message = message;
+        return this;
+    }
+
+    public ConfirmDialogFragment setTitle(final int titleResId) {
+        this.title = sContext.getString(titleResId);
+        return this;
+    }
+
+    public ConfirmDialogFragment setMessage(final int messageResId) {
+        this.message = sContext.getString(messageResId);
         return this;
     }
 
